e78992355c1ca92ae031c7ae581cc3a98fbbdc1b
Nathália Elen <nathaliaelen79@gmail.com>
2023-02-23 21:31:01 -0300

    Condicionais
    Conhecendo as estruturas condicionais simples, compostas e aninhadas.

---
+++ praticas_curso/moduloD_condicionais/1condicionais_simples.js
@@ -0,0 +1,8 @@
+//condicional simples
+let velocidade = 70;
+
+console.log(`A velocidade do seu carro é ${velocidade}km/h`);
+if (velocidade > 60) {
+    console.log('Você ultrapassou a velocidade permitida. MULTADO!');
+} 
+console.log('Dirija sempre com o cinto de segurança');
---
+++ praticas_curso/moduloD_condicionais/2condicionais_composta.js
@@ -0,0 +1,9 @@
+//condicional composta
+let pais = 'EUA'
+
+console.log(`Você vive no(a) ${pais}!`);
+if (pais == 'Brasil') {
+    console.log('Você é Brasileiro!');
+} else {
+    console.log('Você é Estrangeiro!');
+}
